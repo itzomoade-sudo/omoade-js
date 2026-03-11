4945fcd07d586062a2619d1ef406d8eb262588f3
Yusuf Tajudeen <itzomoade@gmail.com>
2026-03-11 22:38:49 +0100

    assignment

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Welcome to user API!'));
+app.get('/', (req, res) => res.send('My Week2 API!'));
 
 app.post('/register', (req, res) => {
     const { name, email, password } = req.body;
-    if (!name || !email || !password) return res.status(400).json({ error: 'Missing fields'});
-    res.status(201).json({ message: `Registered: ${name} ${email} and your password is(${password})` });
+    if (!name || !email) return res.status(400).json({ error: 'Missing fields'});
+    res.status(201).json({ message: `Hello ${name} ${email}` });
 });
 
 app.get('/user/:id',(req, res) => {
-    res.json({ id: req.params.id, name: 'Sample User' });
+    res.json({ id: req.params.id, name: 'item' });
 });
  
 app.listen(3000, () => console.log('API live on port 3000'));
