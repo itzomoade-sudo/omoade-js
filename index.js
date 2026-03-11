a441d44a774491a1211347bfc2ee9f7173a9e950
Yusuf Tajudeen <itzomoade@gmail.com>
2026-03-11 21:48:50 +0100

    express

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ app.use(express.json());
 app.get('/', (req, res) => res.send('Welcome to user API!'));
 
 app.post('/register', (req, res) => {
-    const { name, email } = req.body;
-    if (!name || !email) return res.status(400).json({ error: 'Missing fields'});
-    res.status(201).json({ message: `Registered: ${name} (${email})` });
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) return res.status(400).json({ error: 'Missing fields'});
+    res.status(201).json({ message: `Registered: ${name} ${email} and your password is(${password})` });
 });
 
 app.get('/user/:id',(req, res) => {
     res.json({ id: req.params.id, name: 'Sample User' });
 });
-
+ 
 app.listen(3000, () => console.log('API live on port 3000'));
     
